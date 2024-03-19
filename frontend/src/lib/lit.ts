eb4c18331426e04b7255c658e070650a4279f972
Pileks <pilex13@gmail.com>
2024-03-19 17:42:28 +0100

    lit protocol testing

---
+++ frontend/src/lib/lit.ts
@@ -0,0 +1,82 @@
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
+
+const client = new LitJsSdk.LitNodeClient({
+  litNetwork: "cayenne",
+});
+const chain = "sepolia";
+
+class Lit {
+  private litNodeClient: any;
+
+  async connect() {
+    await client.connect();
+    this.litNodeClient = client;
+  }
+
+  async encrypt(message: string) {
+    if (!this.litNodeClient) {
+      await this.connect();
+    }
+
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({
+      chain: chain,
+      nonce: "",
+    });
+
+    const accessControlConditions = [
+      {
+        chain: chain,
+        method: "eth_getBalance",
+        contractAddress: "",
+        standardContractType: "",
+        parameters: [":userAddress", "latest"],
+        returnValueTest: { comparator: ">=", value: "1" },
+      },
+    ];
+
+    const { ciphertext, dataToEncryptHash } = await LitJsSdk.encryptString(
+      {
+        authSig,
+        chain: chain,
+        dataToEncrypt: message,
+        accessControlConditions,
+      },
+      this.litNodeClient
+    );
+
+    return {
+      ciphertext,
+      dataToEncryptHash,
+      accessControlConditions
+    };
+  }
+
+  async decrypt(
+    ciphertext: string,
+    dataToEncryptHash: string,
+    accessControlConditions: any
+  ) {
+    if (!this.litNodeClient) {
+      await this.connect();
+    }
+
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({
+      chain: chain,
+      nonce: "",
+    });
+    console.log(authSig);
+    const decryptedString = await LitJsSdk.decryptToString(
+      {
+        accessControlConditions,
+        ciphertext,
+        dataToEncryptHash,
+        authSig,
+        chain: chain,
+      },
+      this.litNodeClient
+    );
+    return { decryptedString };
+  }
+}
+
+export default new Lit();
