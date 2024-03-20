0936d5563a4e3a6cb5647b510fbc2d68573b3a0a
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-20 21:17:31 +0100

    removed useless abstract contract, fixed remappings

--- contracts/utils/Security.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-only
-pragma solidity ^0.8.20;
-
-abstract contract Security {
-    modifier onlyTokenOwner(uint256 tokenId, mapping(uint256 tokenId => address) storage owners) {
-        require(msg.sender == owners[tokenId], "Unauthoritzed. Must be token owner.");
-        _;
-    }
-}
