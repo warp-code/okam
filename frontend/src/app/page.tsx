70cdc0b3890174f122fbe351ee66ed95ddaf540d
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-20 18:58:47 +0100

    upload file to nft.storage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,16 @@
+import NftStorageExample from './_examples/NftStorageExample';
 import LitExample from "./_examples/LitExample";
 import WagmiExample from "./_examples/WagmiExample";
 
 export default function Home() {
+  const nftStorageApiKey = process.env.NFT_STORAGE_API_KEY;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <WagmiExample></WagmiExample>
         <LitExample></LitExample>
+        <NftStorageExample token={nftStorageApiKey}></NftStorageExample>
       </div>
     </main>
   );
