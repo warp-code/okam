28975f86df7f59d77218dd40c9908524d13ac7a8
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-20 21:18:36 +0100

    removed useless functions, fixed struct

--- contracts/src/OwnershipToken.sol
+++ contracts/src/OwnershipToken.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Security} from "utils/Security.sol";
 
-contract OwnershipToken is ERC721, Ownable, Security {
-    // replace files type with file struct
+contract OwnershipToken is ERC721, Ownable {
     struct TokenDetails {
         CurveParams curveParams;
-        uint256 files;
+        string fileCid;
     }
 
     struct CurveParams {
@@ -18,24 +16,21 @@ contract OwnershipToken is ERC721, Ownable, Security {
         uint256 constantParam;
     }
 
-    // don't know what goes here yet
-    struct File {
-        uint256 id;
-        string something;
-    }
-
     uint256 _nextTokenId = 0;
 
     mapping(uint256 tokenId => TokenDetails) _details;
 
     constructor(address creator) ERC721("OwnerToken", "OWN") Ownable(creator) {}
 
-    function registerOwner(uint256 quadraticParam, uint256 linearParam, uint256 constantParam) external returns (uint256) {
+    function registerOwner(uint256 quadraticParam, uint256 linearParam, uint256 constantParam, string calldata fileCid)
+        external
+        returns (uint256)
+    {
         uint256 tokenId = _nextTokenId++;
 
         CurveParams memory _params = CurveParams(quadraticParam, linearParam, constantParam);
 
-        _details[tokenId] = TokenDetails(_params, 0);
+        _details[tokenId] = TokenDetails(_params, fileCid);
 
         _safeMint(_msgSender(), tokenId);
 
@@ -53,15 +48,11 @@ contract OwnershipToken is ERC721, Ownable, Security {
         );
     }
 
-    function getFiles(uint256 tokenId) external view returns (uint256) {
+    function getFileCid(uint256 tokenId) external view returns (string memory) {
         if (!_isAuthorized(ownerOf(tokenId), msg.sender, tokenId)) {
             revert ERC721IncorrectOwner(msg.sender, tokenId, ownerOf(tokenId));
         }
 
-        return _details[tokenId].files;
+        return _details[tokenId].fileCid;
     }
-
-    function appendFiles() external {}
-
-    function mintOwnershipToken() external {}
 }
