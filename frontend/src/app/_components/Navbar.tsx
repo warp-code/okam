8a71b189c69ed9aa7a3e2160a0aee51c3aacbfcc
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-21 16:32:10 +0100

    init navbar

---
+++ frontend/src/app/_components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <div className="flex h-18 w-full px-16 items-center">
+            <span className="text-lg font-bold text-green-500">OKAM</span>
+        </div>
+    );
+}
