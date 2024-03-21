a884b9f09228b6f24453e6c8953883d325d4558c
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-21 23:18:51 +0100

    categories and placeholder image

---
+++ frontend/src/app/_constants/categories.ts
@@ -0,0 +1,34 @@
+export const categories = [
+  {
+    id: 1,
+    text: "Audio generation"
+  },
+  {
+    id: 2,
+    text: "Image recognition"
+  },
+  {
+    id: 3,
+    text: "Transcription"
+  },
+  {
+    id: 4,
+    text: "Image generation"
+  },
+  {
+    id: 5,
+    text: "Text prediction"
+  },
+  {
+    id: 6,
+    text: "Medical"
+  },
+  {
+    id: 7,
+    text: "Market data"
+  },
+  {
+    id: 8,
+    text: "Audio generation"
+  },
+] as const;
