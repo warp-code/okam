380faf5c0c0305637b60965c99e9d3d079b44fa1
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-21 16:33:07 +0100

    removed examples from homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,8 @@
-import WriteContract from "./_examples/CallContract";
-import NftStorageExample from './_examples/NftStorageExample';
-import LitExample from "./_examples/LitExample";
-import ReadContract from "./_examples/ReadContract";
-import WagmiExample from "./_examples/WagmiExample";
 
 export default function Home() {
-  const nftStorageApiKey = process.env.NFT_STORAGE_API_KEY;
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <WagmiExample></WagmiExample>
-        <ReadContract></ReadContract>
-        <WriteContract></WriteContract>
-        <LitExample></LitExample>
-        <NftStorageExample token={nftStorageApiKey}></NftStorageExample>
       </div>
     </main>
   );
