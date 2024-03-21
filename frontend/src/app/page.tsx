1be343d22b521870b8dcc361b07135cf095edc0c
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-21 23:17:40 +0100

    home page wip

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,31 @@
+import Category from '@/app/_components/Category';
+import SearchBar from '@/app/_components/SearchBar';
+import { categories } from '@/app/_constants/categories';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <div className="h-full max-w-270 flex flex-col gap-y-12 mx-auto">
+      <div className="min-w-full text-center">
+        <h1 className="text-green-400 font-semibold text-4xl/11 pb-6">
+          Find ML training data
+        </h1>
+
+        <SearchBar name="search" placeholder="I am searching for..." />
+
+        <div className="flex flex-row flex-wrap py-4 gap-3">
+          {
+            categories.map(category => {
+              return (
+                <Category key={category.id} name={category.text} />
+              );
+            })
+          }
+        </div>
+
+        <div className="py-8">
+
+        </div>
       </div>
-    </main>
+    </div >
   );
 }
