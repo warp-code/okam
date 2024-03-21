7aef8064b38d91afb24ecd8158418f5ff26a60c3
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-21 23:57:28 +0100

    added test dataset

---
+++ frontend/src/app/_constants/datasets.ts
@@ -0,0 +1,43 @@
+import placeholderGreen from '@/assets/images/placeholder_green.png';
+import placeholderGreen2 from '@/assets/images/placeholder_green_2.png';
+import placeholderMix from '@/assets/images/placeholder_mix.png';
+import placeholderPink from '@/assets/images/placeholder_pink.png';
+import placeholderViolet from '@/assets/images/placeholder_violet.png';
+
+export const datasets = [
+  {
+    id: 1,
+    image: placeholderGreen,
+    title: "Title training data",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+    price: 2300
+  },
+  {
+    id: 2,
+    image: placeholderGreen2,
+    title: "Title training data",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+    price: 2300
+  },
+  {
+    id: 3,
+    image: placeholderMix,
+    title: "Title training data",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+    price: 2300
+  },
+  {
+    id: 4,
+    image: placeholderPink,
+    title: "Title training data",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+    price: 2300
+  },
+  {
+    id: 5,
+    image: placeholderViolet,
+    title: "Title training data",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+    price: 2300
+  },
+] as const;
