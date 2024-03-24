b59789f295a59c9e55f2dd67f6646a7108515556
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-24 16:59:35 +0100

    replaced react hooks with useQuery

--- frontend/src/app/details/[datasetId]/page.tsx
+++ frontend/src/app/details/[datasetId]/page.tsx
@@ -1,36 +1,42 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { Dataset, datasets } from "@/app/_examples/datasets";
 import Image, { StaticImageData } from "next/image";
 import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Details() {
-  const [dataset, setDataset] = useState<Dataset>();
   const params = useParams();
 
-  useEffect(() => {
-    const x = datasets.find(
+  const { isPending, error, data } = useQuery({
+    queryKey: ["datasets"],
+    queryFn: findDataset,
+  });
+
+  function findDataset(): Dataset | undefined {
+    return datasets.find(
       (x) => x.id === Number.parseInt(params.datasetId as string)
     );
+  }
 
-    setDataset(x);
-  }, [params.datasetId]);
+  if (error) {
+    return console.error(error);
+  }
 
   return (
     <div className="h-full max-w-270 flex flex-col gap-y-12 mx-auto">
-      <div className="min-w-full text-center">
-        {dataset && (
+      <div className="min-w-full">
+        {!isPending && (
           <>
-            <h2 className="text-gray-50 font-semibold text-3xl pb-6 text-left">
-              {dataset?.title}
+            <h2 className="text-gray-50 font-semibold text-3xl pb-6">
+              {data?.title}
             </h2>
 
-            <div className="flex sm:flex-row flex-col w-full sm:gap-x-8 gap-y-8">
-              <div className="flex flex-col gap-y-4 max-w-131">
+            <div className="flex md:flex-row flex-col md:gap-x-8 gap-y-8">
+              <div className="max-w-131 rounded-lg flex flex-col gap-y-4">
                 <Image
-                  alt={dataset?.title as string}
-                  src={dataset?.image as StaticImageData}
+                  alt={data?.title as string}
+                  src={data?.image as StaticImageData}
                   width={524}
                   height={256}
                   className="rounded-lg"
@@ -40,7 +46,7 @@ export default function Details() {
                   Description
                 </h4>
 
-                <div className="text-gray-600">{dataset?.description}</div>
+                <div className="text-gray-400">{data?.description}</div>
 
                 <h4 className="text-gray-50 font-semibold text-2xl">Files</h4>
 
@@ -49,7 +55,7 @@ export default function Details() {
                     <span className="btn btn-xs btn-secondary my-auto">
                       IPFS
                     </span>
-                    <span className="text-gray-600 break-all my-auto">
+                    <span className="text-gray-400 break-all my-auto">
                       QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR
                     </span>
                   </div>
@@ -58,7 +64,7 @@ export default function Details() {
                     <span className="btn btn-xs btn-secondary my-auto">
                       IPFS
                     </span>
-                    <span className="text-gray-600 break-all my-auto">
+                    <span className="text-gray-400 break-all my-auto">
                       QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR
                     </span>
                   </div>
@@ -96,7 +102,7 @@ export default function Details() {
                       />
                     </svg>
                   </span>
-                  <span className="text-gray-600 break-all my-auto">
+                  <span className="text-gray-400 break-all my-auto">
                     0x1c35d2C91E1F2Dc3BA949De78Da32f3FdA5c9863
                   </span>
                 </div>
@@ -104,20 +110,20 @@ export default function Details() {
 
               <div className="max-h-115 max-w-131 rounded-lg flex flex-col">
                 <Image
-                  alt={dataset?.title as string}
-                  src={dataset?.image as StaticImageData}
+                  alt={data?.title as string}
+                  src={data?.image as StaticImageData}
                   width={524}
                   height={256}
                   className="rounded-t-lg"
                 />
                 <div className="max-w-131 rounded-b-lg pt-6 pb-8 px-6 flex flex-col gap-y-4 bg-okam-dark-green">
-                  <div className="text-green-500 text-sm/7 font-semibold text-left">
-                    Current supply: 120
+                  <div className="text-green-500 text-sm/7 font-semibold">
+                    Current supply: {data?.currentSupply}
                   </div>
 
                   <div className="flex flex-row justify-between">
                     <span className="text-gray-50 font-medium py-4">
-                      Buy price: {dataset?.price} FIL
+                      Buy price: {data?.buyPrice} FIL
                     </span>
 
                     <button
@@ -130,7 +136,7 @@ export default function Details() {
 
                   <div className="flex flex-row justify-between">
                     <span className="text-gray-50 font-medium py-4">
-                      Sell price: {dataset?.price} FIL
+                      Sell price: {data?.sellPrice} FIL
                     </span>
 
                     <button
