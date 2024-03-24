afe61c72092566c5be8339013126b62984b0dc86
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-24 18:01:58 +0100

    created template, added routing to homepage

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,19 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers>
         <body className={inter.className && "flex flex-col min-h-screen"}>
-          <header className="flex h-18 w-full sm:px-16 px-8 items-center">
-            <span className="text-lg font-bold text-green-500">OKAM</span>
-          </header>
-
-          <main className="w-full flex-grow sm:px-16 px-8 py-8">
-            {children}
-          </main>
-
-          <footer className="flex h-18 w-full sm:px-16 px-8 items-center text-sm">
-            <span className="text-green-500 font-semibold">OKAM</span>
-            &nbsp;
-            <span className="text-white font-medium">by WarpCode</span>
-          </footer>
+          {children}
         </body>
       </Providers>
     </html>
---
+++ frontend/src/app/template.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <>
+      <header className="flex h-18 w-full sm:px-16 px-8 items-center">
+        <span
+          className="text-lg font-bold text-green-500 cursor-pointer"
+          onClick={() => router.push("/")}
+        >
+          OKAM
+        </span>
+      </header>
+
+      <main className="w-full flex-grow sm:px-16 px-8 py-8">{children}</main>
+
+      <footer className="flex h-18 w-full sm:px-16 px-8 items-center text-sm">
+        <span className="text-green-500 font-semibold">OKAM</span>
+        &nbsp;
+        <span className="text-white font-medium">by WarpCode</span>
+      </footer>
+    </>
+  );
+}
