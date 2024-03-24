8d89d08670b8860812afdca9eaee10677bb144f2
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-24 12:40:51 +0100

    details page wip

--- frontend/src/app/details/[datasetId]/page.tsx
+++ frontend/src/app/details/[datasetId]/page.tsx
@@ -24,7 +24,7 @@ export default function Details() {
           {dataset?.title}
         </h2>
 
-        <div className="flex flex-row sm:gap-x-8 gap-y-8">
+        <div className="flex sm:flex-row flex-col w-full sm:gap-x-8 gap-y-8">
           <div className="flex flex-col gap-y-4 max-w-131">
             <Image
               alt={dataset?.title as string}
@@ -42,15 +42,15 @@ export default function Details() {
 
             <div className="flex flex-col gap-y-3">
               <div className="flex flex-row gap-x-2.5">
-                <span className="btn btn-xs btn-secondary">IPFS</span>
-                <span className="text-gray-600">
+                <span className="btn btn-xs btn-secondary my-auto">IPFS</span>
+                <span className="text-gray-600 break-all">
                   QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR
                 </span>
               </div>
 
               <div className="flex flex-row gap-x-2.5">
-                <span className="btn btn-xs btn-secondary">IPFS</span>
-                <span className="text-gray-600">
+                <span className="btn btn-xs btn-secondary my-auto">IPFS</span>
+                <span className="text-gray-600 break-all">
                   QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR
                 </span>
               </div>
@@ -59,14 +59,47 @@ export default function Details() {
             <h4 className="text-gray-50 font-semibold text-2xl">Author</h4>
 
             <div className="flex flex-row gap-x-2.5">
-              <span className="btn btn-xs btn-secondary">IPFS</span>
-              <span className="text-gray-600">
+              <span className="btn btn-xs btn-secondary my-auto">IPFS</span>
+              <span className="text-gray-600 break-all">
                 0x1c35d2C91E1F2Dc3BA949De78Da32f3FdA5c9863
               </span>
             </div>
           </div>
 
-          <div className="flex flex-col"></div>
+          <div className="max-h-115 max-w-131 rounded-lg flex flex-col">
+            <Image
+              alt={dataset?.title as string}
+              src={dataset?.image as StaticImageData}
+              width={524}
+              height={256}
+              className="rounded-t-lg"
+            />
+            <div className="max-w-131 rounded-b-lg pt-6 pb-8 px-6 flex flex-col gap-y-4 bg-okam-dark-green">
+              <div className="text-green-500 text-sm/7 font-semibold text-left">
+                Current supply: 120
+              </div>
+
+              <div className="flex flex-row justify-between">
+                <span className="text-gray-50 font-medium py-4">
+                  Buy price: {dataset?.price} FIL
+                </span>
+
+                <button className="btn btn-md btn-primary my-auto min-w-25">
+                  Buy
+                </button>
+              </div>
+
+              <div className="flex flex-row justify-between">
+                <span className="text-gray-50 font-medium py-4">
+                  Sell price: {dataset?.price} FIL
+                </span>
+
+                <button className="btn btn-md btn-tertiary my-auto min-w-25">
+                  Sell
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
