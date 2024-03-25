e234fb61f26cb5427d6895fb2a56e03ddb617b4c
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-25 16:23:53 +0100

    simplified text input

--- frontend/src/app/_components/TextInput.tsx
+++ frontend/src/app/_components/TextInput.tsx
@@ -1,21 +1,21 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 export default function TextInput({
-  id,
+  name,
+  value,
   label,
   placeholder,
+  handleOnChange,
 }: {
-  id: string;
+  name: string;
+  value: any;
   label?: string | undefined;
   placeholder?: string | undefined;
+  handleOnChange: Function;
 }) {
   return (
     <>
       {label && (
         <label
-          htmlFor={id}
+          htmlFor={name}
           className="text-gray-50 text-lg text-left cursor-pointer"
         >
           {label}
@@ -24,9 +24,11 @@ export default function TextInput({
 
       <input
         type="text"
-        id={id}
-        name={id}
+        id={name}
+        name={name}
+        value={value}
         placeholder={placeholder}
+        onChange={(e) => handleOnChange(e.target.value)}
         className="block w-full border border-green-700 focus:border-green-400 focus:outline-none rounded-2xl px-6 py-4 bg-okam-dark-green placeholder:text-gray-400 text-gray-50"
       />
     </>
