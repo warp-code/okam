dd03cb3f69808c18220bf510ae4623a7539d70d9
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-25 16:23:30 +0100

    supress hydration warnings

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className && "flex flex-col min-h-screen"}>
+        <body
+          className={inter.className && "flex flex-col min-h-screen"}
+          suppressHydrationWarning={true}
+        >
           {children}
         </body>
       </Providers>
