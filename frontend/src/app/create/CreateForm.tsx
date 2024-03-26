b63dd8ffe0e59bad78b54f4d3f06454799afc0a5
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-26 16:41:04 +0100

    create form wip

--- frontend/src/app/create/CreateForm.tsx
+++ frontend/src/app/create/CreateForm.tsx
@@ -40,167 +40,141 @@ export default function CreateForm({
         : (categoriesQuery.data?.map((x) => {
             return { id: x.id, text: x.text, checked: false };
           }) as Array<{ id: number; text: string; checked: boolean }>),
-      files: [
-        {
-          name: null,
-          mimeType: null,
-          cid: "",
-        },
-      ] as Array<{
+      file: {
+        name: null,
+        mimeType: null,
+        cid: "",
+      } as {
         name: string | null;
         mimeType: string | null;
         cid: string | null;
-      }>,
+      },
     },
     onSubmit: (e) => handleSubmit(e),
   });
 
   return (
-    <div className="h-full max-w-192 flex flex-col mx-auto">
-      <form
-        className="flex flex-col min-w-full text-center gap-y-6"
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
+    <form
+      className="flex flex-col min-w-full text-center gap-y-6"
+      onSubmit={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
 
-          void form.handleSubmit();
-        }}
-      >
-        <h2 className="text-gray-50 font-semibold text-3xl/9.5 pb-6 text-left">
-          Create dataset
-        </h2>
+        void form.handleSubmit();
+      }}
+    >
+      <h2 className="text-gray-50 font-semibold text-3xl/9.5 pb-6 text-left">
+        Create dataset
+      </h2>
 
-        <form.Field name="name">
-          {(field) => (
-            <TextInput
-              label="Name"
-              name={field.name}
-              value={field.state.value}
-              handleOnChange={(event) => field.handleChange(event.target.value)}
-            />
-          )}
-        </form.Field>
+      <form.Field name="name">
+        {(field) => (
+          <TextInput
+            label="Name"
+            name={field.name}
+            value={field.state.value}
+            handleOnChange={(event) => field.handleChange(event.target.value)}
+          />
+        )}
+      </form.Field>
 
-        <form.Field name="coverImage">
-          {(field) => (
-            <ImageUploader
-              label="Cover image"
-              name={field.name}
-              value={field.state.value}
-              nftStorageApiKey={nftStorageApiKey}
-              handleOnChange={(event: any) => field.handleChange(event)}
-            />
-          )}
-        </form.Field>
+      <form.Field name="coverImage">
+        {(field) => (
+          <ImageUploader
+            label="Cover image"
+            name={field.name}
+            value={field.state.value}
+            nftStorageApiKey={nftStorageApiKey}
+            handleOnChange={(event: any) => field.handleChange(event)}
+          />
+        )}
+      </form.Field>
 
-        <form.Field name="description">
-          {(field) => (
-            <TextareaInput
-              label="Description"
-              name={field.name}
-              value={field.state.value}
-              handleOnChange={(event) => field.handleChange(event.target.value)}
-            />
-          )}
-        </form.Field>
+      <form.Field name="description">
+        {(field) => (
+          <TextareaInput
+            label="Description"
+            name={field.name}
+            value={field.state.value}
+            handleOnChange={(event) => field.handleChange(event.target.value)}
+          />
+        )}
+      </form.Field>
 
-        <h4 className="text-gray-50 text-lg text-left">Tags</h4>
+      <h4 className="text-gray-50 text-lg text-left">Tags</h4>
 
-        <div className="flex flex-row flex-wrap py-4 gap-3">
-          <form.Field name="categories" mode="array">
-            {(field) =>
-              field.state.value.map((category, i) => (
-                <form.Field key={i} name={`categories[${i}].checked`}>
-                  {(subField) => {
-                    return (
-                      <CategoryCheckbox
-                        name={subField.name}
-                        label={category.text}
-                        value={subField.state.value}
-                        handleOnChange={(event) =>
-                          subField.handleChange(event.target.checked)
-                        }
-                      />
-                    );
-                  }}
-                </form.Field>
-              ))
-            }
-          </form.Field>
-        </div>
-
-        <h4 className="text-gray-50 text-lg text-left">Files</h4>
-
-        <div className="flex flex-col gap-y-6">
-          <form.Field name="files" mode="array">
-            {(field) =>
-              field.state.value.map((file, i) => (
-                <form.Field key={i} name={"files"}>
-                  {(subField) => (
-                    <FileUploader
-                      name={`${subField.name}-${i}`}
-                      value={subField.state.value[i]}
-                      nftStorageApiKey={nftStorageApiKey}
-                      handleOnChange={(event: any) => field.handleChange(event)}
+      <div className="flex flex-row flex-wrap py-4 gap-3">
+        <form.Field name="categories" mode="array">
+          {(field) =>
+            field.state.value.map((category, i) => (
+              <form.Field key={i} name={`categories[${i}].checked`}>
+                {(subField) => {
+                  return (
+                    <CategoryCheckbox
+                      name={subField.name}
+                      label={category.text}
+                      value={subField.state.value}
+                      handleOnChange={(event) =>
+                        subField.handleChange(event.target.checked)
+                      }
                     />
-                  )}
-                </form.Field>
-              ))
-            }
-          </form.Field>
+                  );
+                }}
+              </form.Field>
+            ))
+          }
+        </form.Field>
+      </div>
 
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row gap-x-2.5">
-              <span className="btn btn-xs btn-secondary my-auto select-none">
-                IPFS
-              </span>
-              <span className="text-gray-50 break-all my-auto">
-                QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR
-              </span>
-            </div>
-            <button type="button" className="text-gray-400 text-sm">
-              Delete
+      <div className="flex flex-col gap-y-6">
+        <form.Field name="file">
+          {(field) => (
+            <>
+              <FileUploader
+                label="File"
+                name={field.name}
+                value={field.state.value}
+                nftStorageApiKey={nftStorageApiKey}
+                handleOnChange={(file: any) => field.handleChange(file)}
+              />
+
+              {field.state.value.name && (
+                <button
+                  type="button"
+                  className="text-gray-400 text-sm"
+                  onClick={() =>
+                    field.setValue({ name: null, cid: null, mimeType: null })
+                  }
+                >
+                  Delete
+                </button>
+              )}
+            </>
+          )}
+        </form.Field>
+      </div>
+
+      <div className="flex justify-end gap-x-2.5 py-2.5">
+        <button
+          type="button"
+          className="btn btn-sm btn-secondary"
+          onClick={() => push("/")}
+        >
+          Cancel
+        </button>
+
+        <form.Subscribe>
+          {(formState) => (
+            <button
+              type="submit"
+              className="btn btn-sm btn-primary"
+              disabled={!formState.canSubmit}
+            >
+              Create
             </button>
-          </div>
-
-          <div className="flex flex-row  justify-between">
-            <div className="flex flex-row gap-x-2.5">
-              <span className="btn btn-xs btn-secondary my-auto select-none">
-                IPFS
-              </span>
-              <span className="text-gray-50 break-all my-auto">
-                QmbWqxBEKC3P8tqsKc98xmWNzrzDtRLMiMPL8wBuTGsMnR
-              </span>
-            </div>
-
-            <button type="button" className="text-gray-400 text-sm">
-              Delete
-            </button>
-          </div>
-        </div>
-
-        <div className="flex justify-end gap-x-2.5 py-2.5">
-          <button
-            type="button"
-            className="btn btn-sm btn-secondary"
-            onClick={() => push("/")}
-          >
-            Cancel
-          </button>
-
-          <form.Subscribe>
-            {(formState) => (
-              <button
-                type="submit"
-                className="btn btn-sm btn-primary"
-                disabled={!formState.canSubmit}
-              >
-                Create
-              </button>
-            )}
-          </form.Subscribe>
-        </div>
-      </form>
-    </div>
+          )}
+        </form.Subscribe>
+      </div>
+    </form>
   );
 }
