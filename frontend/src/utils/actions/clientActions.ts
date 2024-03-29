c2dbf0e5c3fad68c4baa65facc9c2c9e5d2e6665
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-29 13:27:11 +0100

    formatting, return zero if quantity is zero

--- frontend/src/utils/actions/clientActions.ts
+++ frontend/src/utils/actions/clientActions.ts
@@ -1,9 +1,17 @@
 "use client";
 
 export function calculateBuyPrice(quadraticParam: number, linearParam: number, constantParam: number, quantity: number): number {
-    return Math.floor((quadraticParam * Math.pow(quantity, 2)) + (linearParam * quantity) + constantParam);
+  if (quantity === 0) {
+    return 0;
+  }
+
+  return Math.floor((quadraticParam * Math.pow(quantity, 2)) + (linearParam * quantity) + constantParam);
 }
 
 export function calculateSellPrice(buyPrice: number): number {
-    return Math.floor(buyPrice * 9 / 10);
+  if (buyPrice === 0) {
+    return 0;
+  }
+
+  return Math.floor(buyPrice * 9 / 10);
 }
