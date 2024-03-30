fb63f4e4493a4a8d31f91148c984a6700806ec62
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-30 19:36:27 +0100

    removed useless import

--- contracts/src/OwnershipToken.sol
+++ contracts/src/OwnershipToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.20;
 
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract OwnershipToken is ERC721 {
