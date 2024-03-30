b36f4cb98e37f7a8cec0f8d800eeec9826609d4c
Dujo Perdic <dujo.perdic1@gmail.com>
2024-03-30 17:29:45 +0100

    added access token actions, wip

--- frontend/src/contracts/actions.ts
+++ frontend/src/contracts/actions.ts
@@ -1,20 +1,25 @@
 "use client";
 
 import { wagmiConfig } from "@/lib/config";
-import { waitForTransactionReceipt, writeContract } from "wagmi/actions";
-import { ownershipTokenAbi } from "./ownershipTokenAbi";
-import { decodeEventLog } from "viem";
+import { accessTokenAbi } from "@/contracts/accessTokenAbi";
+import { ownershipTokenAbi } from "@/contracts/ownershipTokenAbi";
+import {
+  waitForTransactionReceipt,
+  writeContract,
+  readContract,
+} from "wagmi/actions";
 import { useState } from "react";
+import { decodeEventLog } from "viem";
 
 type Result<T, E = any> =
   | {
-    ok: true;
-    data: T;
-  }
+      ok: true;
+      data: T;
+    }
   | {
-    ok: false;
-    error: E;
-  };
+      ok: false;
+      error: E;
+    };
 
 export function useMintOwnershipToken() {
   const [loading, setLoading] = useState(false);
@@ -63,6 +68,100 @@ export async function mintOwnershipToken(fileCid: string): Promise<string> {
 
   if (!tokenId) {
     throw "No Transfer event was emitted as part of Ownership token minting.";
+  }
+
+  return tokenId;
+}
+
+export async function getSupply(ownerhipTokenId: bigint) {
+  return await readContract(wagmiConfig, {
+    abi: accessTokenAbi,
+    address: process.env.NEXT_PUBLIC_ACCESS_CONTRACT_ADDRESS,
+    functionName: "getSupply",
+    args: [ownerhipTokenId],
+  });
+}
+
+export async function getBuyPrice(ownerhipTokenId: bigint) {
+  return await readContract(wagmiConfig, {
+    abi: accessTokenAbi,
+    address: process.env.NEXT_PUBLIC_ACCESS_CONTRACT_ADDRESS,
+    functionName: "buyPrice",
+    args: [ownerhipTokenId],
+  });
+}
+
+export async function getSellPrice(ownerhipTokenId: bigint) {
+  return await readContract(wagmiConfig, {
+    abi: accessTokenAbi,
+    address: process.env.NEXT_PUBLIC_ACCESS_CONTRACT_ADDRESS,
+    functionName: "sellPrice",
+    args: [ownerhipTokenId],
+  });
+}
+
+export async function mintAccessToken(
+  ownerhipTokenId: bigint,
+  address: `0x${string}`
+) {
+  const txHash = await writeContract(wagmiConfig, {
+    abi: accessTokenAbi,
+    address: process.env.NEXT_PUBLIC_ACCESS_CONTRACT_ADDRESS,
+    functionName: "mint",
+    args: [ownerhipTokenId, address],
+  });
+
+  const txReceipt = await waitForTransactionReceipt(wagmiConfig, {
+    hash: txHash,
+    confirmations: 1,
+  });
+
+  let tokenId: string | undefined = undefined;
+
+  for (const log of txReceipt.logs) {
+    const transferEvent = decodeEventLog({
+      abi: accessTokenAbi,
+      topics: log.topics,
+      data: log.data,
+      eventName: "Transfer",
+    });
+    tokenId = transferEvent.args.tokenId.toString();
+  }
+
+  if (!tokenId) {
+    throw "No Transfer event was emitted as part of Access token minting.";
+  }
+
+  return tokenId;
+}
+
+export async function burnAccessToken(tokenIdToBurn: bigint) {
+  const txHash = await writeContract(wagmiConfig, {
+    abi: accessTokenAbi,
+    address: process.env.NEXT_PUBLIC_ACCESS_CONTRACT_ADDRESS,
+    functionName: "burn",
+    args: [tokenIdToBurn],
+  });
+
+  const txReceipt = await waitForTransactionReceipt(wagmiConfig, {
+    hash: txHash,
+    confirmations: 1,
+  });
+
+  let tokenId: string | undefined = undefined;
+
+  for (const log of txReceipt.logs) {
+    const transferEvent = decodeEventLog({
+      abi: accessTokenAbi,
+      topics: log.topics,
+      data: log.data,
+      eventName: "Transfer",
+    });
+    tokenId = transferEvent.args.tokenId.toString();
+  }
+
+  if (!tokenId) {
+    throw "No Transfer event was emitted as part of Access token burning.";
   }
 
   return tokenId;
