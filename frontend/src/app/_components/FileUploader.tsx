f4548b7dfc643b5e7be9c6c367c1c2e2acdbb581
Dujo Perdic <dujo.perdic1@gmail.com>
2024-04-06 19:00:55 +0200

    added loading indicators to uploaders

--- frontend/src/app/_components/FileUploader.tsx
+++ frontend/src/app/_components/FileUploader.tsx
@@ -3,7 +3,8 @@
 import { uploadFileToIpfs } from "@/utils/actions/serverActions";
 import { OkamFile } from "@/app/types";
 import { ValidationError } from "@tanstack/react-form";
-import { MouseEventHandler } from "react";
+import { MouseEventHandler, useState } from "react";
+import LoadingIndicator from "@/app/_components/LoadingIndicator";
 
 export default function FileUploader({
   name,
@@ -22,6 +23,8 @@ export default function FileUploader({
   disabled?: boolean;
   errors?: ValidationError[];
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+
   const uploadFormData = async (file: File | undefined): Promise<OkamFile> => {
     if (!file) {
       console.error("No file was uploaded.");
@@ -33,11 +36,17 @@ export default function FileUploader({
       } as OkamFile;
     }
 
+    setIsLoading(true);
+
     const formData = new FormData();
 
     formData.append("file", file);
 
-    return await uploadFileToIpfs(formData);
+    const uploadedFile = await uploadFileToIpfs(formData);
+
+    setIsLoading(false);
+
+    return uploadedFile;
   };
 
   return (
@@ -74,37 +83,45 @@ export default function FileUploader({
       ) : (
         <>
           <div className="flex flex-col relative border border-green-800 rounded-lg w-full px-6 py-4 gap-y-3 items-center">
-            <label
-              htmlFor={name}
-              className="items-center border border-green-800 rounded-lg p-2.5 max-w-10 max-h-10 cursor-pointer"
-            >
-              <svg
-                width="20"
-                height="18"
-                viewBox="0 0 20 18"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="text-green-300"
-              >
-                <path
-                  d="M6.66675 12.3333L10.0001 9M10.0001 9L13.3334 12.3333M10.0001 9V16.5M16.6667 12.9524C17.6847 12.1117 18.3334 10.8399 18.3334 9.41667C18.3334 6.88536 16.2814 4.83333 13.7501 4.83333C13.568 4.83333 13.3976 4.73833 13.3052 4.58145C12.2185 2.73736 10.2121 1.5 7.91675 1.5C4.46497 1.5 1.66675 4.29822 1.66675 7.75C1.66675 9.47175 2.36295 11.0309 3.48921 12.1613"
-                  stroke="currentColor"
-                  strokeWidth="1.66667"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-            </label>
+            {isLoading ? (
+              <div className="h-12 w-12 mx-auto m-y-12">
+                <LoadingIndicator />
+              </div>
+            ) : (
+              <>
+                <label
+                  htmlFor={name}
+                  className="items-center border border-green-800 rounded-lg p-2.5 max-w-10 max-h-10 cursor-pointer"
+                >
+                  <svg
+                    width="20"
+                    height="18"
+                    viewBox="0 0 20 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="text-green-300"
+                  >
+                    <path
+                      d="M6.66675 12.3333L10.0001 9M10.0001 9L13.3334 12.3333M10.0001 9V16.5M16.6667 12.9524C17.6847 12.1117 18.3334 10.8399 18.3334 9.41667C18.3334 6.88536 16.2814 4.83333 13.7501 4.83333C13.568 4.83333 13.3976 4.73833 13.3052 4.58145C12.2185 2.73736 10.2121 1.5 7.91675 1.5C4.46497 1.5 1.66675 4.29822 1.66675 7.75C1.66675 9.47175 2.36295 11.0309 3.48921 12.1613"
+                      stroke="currentColor"
+                      strokeWidth="1.66667"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </label>
 
-            <label htmlFor={name} className="cursor-pointer">
-              <span className="text-green-300 select-none">
-                Click to upload
-              </span>
-              &nbsp;
-              <span className="text-gray-400 select-none">
-                or drag and drop
-              </span>
-            </label>
+                <label htmlFor={name} className="cursor-pointer">
+                  <span className="text-green-300 select-none">
+                    Click to upload
+                  </span>
+                  &nbsp;
+                  <span className="text-gray-400 select-none">
+                    or drag and drop
+                  </span>
+                </label>
+              </>
+            )}
 
             <input
               type="file"
