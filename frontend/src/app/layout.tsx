72d9ade1fb139dc9369d937b468a944a14a31424
Dujo Perdic <dujo.perdic1@gmail.com>
2024-04-06 14:56:21 +0200

    replaced font

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import Providers from "@/providers/Providers";
 import "./globals.css";
 import { cookieToInitialState } from "wagmi";
 import { headers } from "next/headers";
 import { wagmiConfig } from "@/lib/config";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  preload: true,
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "OKAM.ai",
@@ -27,7 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <Providers initialState={initialState}>
         <body
-          className={inter.className && "flex flex-col min-h-screen"}
+          className={`${poppins.className} antialiased flex flex-col min-h-screen`}
           suppressHydrationWarning={true}
         >
           {children}
